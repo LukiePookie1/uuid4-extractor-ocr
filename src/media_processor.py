db49b173908d7e16ff0812506c4bd32d2d514778
Luke Rappa <luke@Lukes-MacBook-Pro.local>
2024-09-21 16:59:16 -0400

    Media processor class

--- src/media_processor.py
+++ src/media_processor.py
@@ -1,25 +1,37 @@
 import os
 from typing import List
+from ffmpy import FFmpeg
 import tempfile
+import easyocr
 
 class MediaProcessor: 
     def __init__(self):
-
-    def process_video(self, video_path: str, fps: int = 1) -> str:
-        return
-    
-    def process_image(self, image_path: str) -> str:
-        return
+        self.temp_dir = tempfile.TemporaryDirectory()
     
     def slice_video(self, video_path: str, fps: int = 1) -> List[str]:
-        return
-    
-    def optimize_images(self, path: str) -> List[str]:
-        return
+        ff = FFmpeg(
+            inputs={video_path: None},
+            outputs={os.path.join(self.temp_dir.name, "frame_%03d.png"): f"-vf fps={fps}"}
+        )
+
+        ff.run()
+
+        frame_paths = []
+        for file in os.listdir(self.temp_dir.name):
+            if file.endswith('.png'):
+                frame_paths.append(file)
+        return frame_paths
     
     def detect_text(self, path: str) -> str:
-        return
-    
-    def cleanup(self):
+        reader = easyocr.Reader(['en'])
+
+        result = reader.readtext(path)   
+
+        #Only want text, not bounding boxes or confidence scores
+        text = [text for _, text, _ in result]
+        
+        return "".join(text)        
 
     def __del__(self):
+        self.temp_dir.cleanup()
+        
